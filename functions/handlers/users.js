96a87a6c12f911aff6d6610c59a95d4b961d6998
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2020-03-11 16:47:52 +0530

    commenting out not needed

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -186,35 +186,35 @@ exports.getAuthenticatedUser = (req, res) => {
 }
 
 exports.getUserDetails = (req, res) => {
-  const userData = {}
+  // const userData = {}
   db.doc(`/user/${req.params.handle}`)
     .get()
     .then((doc) => {
-      if (doc.exists) {
-        userData.user = doc.data()
-        return db
-          .collection('post')
-          .where('userHandle', '==', req.params.handle)
-          .orderBy('createdAt', 'desc')
-          .get()
-      } else {
-        return res.status(404).json({ errror: 'User not found' })
-      }
-    })
-    .then((data) => {
-      userData.post = []
-      data.forEach((doc) => {
-        userData.post.push({
-          body: doc.data().body,
-          createdAt: doc.data().createdAt,
-          userHandle: doc.data().userHandle,
-          userImage: doc.data().userImage,
-          likeCount: doc.data().likeCount,
-          commentCount: doc.data().commentCount,
-          postId: doc.id
-        })
-      })
-      return res.json(userData)
+    //   if (doc.exists) {
+    //     userData.user = doc.data()
+    //     return db
+    //       .collection('post')
+    //       .where('userHandle', '==', req.params.handle)
+    //       .orderBy('createdAt', 'desc')
+    //       .get()
+    //   } else {
+    //     return res.status(404).json({ errror: 'User not found' })
+    //   }
+    // })
+    // .then((data) => {
+    //   userData.post = []
+    //   data.forEach((doc) => {
+    //     userData.post.push({
+    //       body: doc.data().body,
+    //       createdAt: doc.data().createdAt,
+    //       userHandle: doc.data().userHandle,
+    //       userImage: doc.data().userImage,
+    //       likeCount: doc.data().likeCount,
+    //       commentCount: doc.data().commentCount,
+    //       postId: doc.id
+    //     })
+    //   })
+      return res.json(doc.data())
     })
     .catch((err) => {
       console.error(err)
