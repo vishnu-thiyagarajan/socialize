241c00c51ab663812d7f6ac3a3ca7ef4e2f5c290
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2020-03-14 12:34:29 +0530

    pagination

--- functions/index.js
+++ functions/index.js
@@ -26,13 +26,13 @@ const {
 
 app.use(cors())
 app.get('/posts', getAllPosts)
-app.get('/posts/:offSet/:limit', getPosts)
 app.post('/posts', FBAuth, postOnePost)
 app.get('/posts/:postId', FBAuth, getPost)
 app.post('/posts/:postId/comment', FBAuth, commentOnPost)
 app.delete('/posts/:postId', FBAuth, deletePost)
 app.get('/posts/:postId/like', FBAuth, likePost)
 app.get('/posts/:postId/unlike', FBAuth, unlikePost)
+app.get('/postpage/:offSet/:limit', getPosts)
 
 app.get('/user', FBAuth, getAuthenticatedUser)
 app.post('/user', FBAuth, addUserDetails)
@@ -42,65 +42,3 @@ app.post('/login', login)
 app.get('/user/:handle', getUserDetails)
 app.post('/notifications', FBAuth, markNotificationsRead)
 exports.api = functions.region('asia-east2').https.onRequest(app)
-
-// exports.createNotificationOnLike = functions
-//   .region('asia-east2')
-//   .firestore.document('likes/{id}')
-//   .onCreate((snapshot) => {
-//     return db
-//       .doc(`/post/${snapshot.data().postId}`)
-//       .get()
-//       .then((doc) => {
-//         if (
-//           doc.exists &&
-//           doc.data().userHandle !== snapshot.data().userHandle
-//         ) {
-//           return db.doc(`/notifications/${snapshot.id}`).set({
-//             createdAt: new Date().toISOString(),
-//             recipient: doc.data().userHandle,
-//             sender: snapshot.data().userHandle,
-//             type: 'like',
-//             read: false,
-//             postId: doc.id
-//           })
-//         }
-//       })
-//       .catch((err) => console.error(err))
-//   })
-// exports.deleteNotificationOnUnLike = functions
-//   .region('asia-east2')
-//   .firestore.document('likes/{id}')
-//   .onDelete((snapshot) => {
-//     return db
-//       .doc(`/notifications/${snapshot.id}`)
-//       .delete()
-//       .catch((err) => {
-//         console.error(err)
-//       })
-//   })
-// exports.createNotificationOnComment = functions
-//   .region('asia-east2')
-//   .firestore.document('comments/{id}')
-//   .onCreate((snapshot) => {
-//     return db
-//       .doc(`/post/${snapshot.data().postId}`)
-//       .get()
-//       .then((doc) => {
-//         if (
-//           doc.exists &&
-//           doc.data().userHandle !== snapshot.data().userHandle
-//         ) {
-//           return db.doc(`/notifications/${snapshot.id}`).set({
-//             createdAt: new Date().toISOString(),
-//             recipient: doc.data().userHandle,
-//             sender: snapshot.data().userHandle,
-//             type: 'comment',
-//             read: false,
-//             postId: doc.id
-//           })
-//         }
-//       })
-//       .catch((err) => {
-//         console.error(err)
-//       })
-//   })
